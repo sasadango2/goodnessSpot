88131bc924ed3e8ce66b4b8ece9f3c141fa82660
Kobayashi Sota <xxiangtai75@gmail.com>
2025-02-04 00:30:08 +0900

    Build a spring boot environment

---
+++ backend/goodnessSpot/src/main/java/com/sta/goodnessSpot/GoodnessSpotApplication.java
@@ -0,0 +1,13 @@
+package com.sta.goodnessSpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodnessSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodnessSpotApplication.class, args);
+	}
+
+}
---
+++ backend/goodnessSpot/src/test/java/com/sta/goodnessSpot/GoodnessSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sta.goodnessSpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodnessSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
